65dfa85096e05ac6ed3bea9b14f4350e4a7d6c8a
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-10-04 22:30:32 +0000

    Clean up test dependency setup.

--- lib/src/test/java/org/opendcs/testing/gherkin/Processing.java
+++ lib/src/test/java/org/opendcs/testing/gherkin/Processing.java
@@ -1,8 +1,13 @@
 package org.opendcs.testing.gherkin;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * NOTE: tests will be created. Placeholder for ci/cd setup at the moment.
  */
 public class Processing {
-    
+    @Test
+    public void test_feature_processing() {
+
+    }
 }
